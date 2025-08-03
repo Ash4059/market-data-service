0cc5fd99e764c3b21bd355a7c6a1680daf8c32d4
Ash4059 <jaiswalashish405@gmail.com>
2025-08-03 08:53:42 +0530

    Initialize repository

---
+++ src/test/java/com/MarketPulse/Market_data_service/MarketDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MarketPulse.Market_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
